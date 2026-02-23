13b63d88d01d838ddf0d0beed960795764f9459a
Siddu-009 <siddardha9988@gmail.com>
2026-02-23 16:28:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-  count                  = 4
-  ami                    = "ami-0360c520857e3138f"
-  instance_type          = "t2.medium"
-  key_name               = "terraformkeypair"
-  vpc_security_group_ids = ["sg-0f1d5f2f18bb4af01"]
+  count                  = 3
+  ami                    = "ami-019715e0d74f695be "
+  instance_type          = "t3.micro"
+  key_name               = "SMUMB"
+  vpc_security_group_ids = ["sg-06bc3b3735fcdd7c0"]
   tags = {
     Name = var.instance_names[count.index]
   }
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+  default = ["tomcat-1", "tomcat-2", "Monitoring server"]
 }
